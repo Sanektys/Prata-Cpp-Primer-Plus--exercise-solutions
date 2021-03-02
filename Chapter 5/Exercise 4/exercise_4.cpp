7e05cd4ed6f58251afd5dcb231d2fdf1b6815a94
Sanektys <Sanektisru@gmail.com>
2021-03-02 19:46:22 +0700

    Add solutions of chapter 5
    Another revert test
    This reverts commit 184e39c55ff231f7427fb0fbc4a61cce5f274ca9.

---
+++ Chapter 5/Exercise 4/exercise_4.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+
+const short DAFNA_PERCENT = 10;
+const short CLEO_PERCENT = 5;
+
+void delay(int);
+
+int main() {
+    double balanceDafna{100};
+    double baseBalanceDafna = balanceDafna;
+    double balanceCleo{100};
+    short year{};
+    do {
+        delay(1);
+        ++year;
+        std::cout << "Sum of balances for year " << year << ":\n";
+        delay(2);
+        balanceDafna += DAFNA_PERCENT / 100.0 * baseBalanceDafna;
+        balanceCleo += CLEO_PERCENT / 100.0 * balanceCleo;
+        std::cout << "Dafna - " << balanceDafna << "  |  "
+                  << "Cleo - " << balanceCleo << std::endl << std::endl;
+    } while (!(balanceCleo > balanceDafna));
+
+
+    return 0;
+}
+
+void delay(int seconds) {
+    clock_t delay = seconds * CLOCKS_PER_SEC;
+    clock_t start = clock();
+    while (clock() - start < delay);
+}
---
+++ Chapter 5/Exercise 5/exercise_5.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+
+int main() {
+    const char* month[12]{
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+    };
+    int amount[12]{};
+
+    std::cout << "Enter the number of books sold for each month\n"
+                 "and the program will summarize the total number\n\n";
+    int totalAmount{};
+    std::cout << "Enter the amount of books sold...\n";
+    for (int i = 0; i < 12; ++i) {
+        std::cout << "\t\t\t\t...in " << *(month + i) << ": ";
+        std::cin >> amount[i];
+        totalAmount += amount[i];
+    }
+    std::cout << "\n\t\aTotal amount of sold books: " << totalAmount << std::endl;
+
+
+    return 0;
+}
