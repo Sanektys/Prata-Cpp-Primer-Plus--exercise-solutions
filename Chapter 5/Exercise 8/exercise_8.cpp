9740972505a8ac578e4029ae84362966adfda47e
Sanektys <Sanektisru@gmail.com>
2021-03-02 19:28:08 +0700

    Add solutions of chapter 5

---
+++ Chapter 5/Exercise 8/exercise_8.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string.h>
+
+
+int main() {
+    setlocale(LC_ALL, "RUS");
+
+    std::wcout << L"Вводите слова (для завершения введите слово done):\n";
+    char word[16]{};
+    short count = 0;
+
+    std::cin >> word;
+    while (strcmp(word, "done")) {
+        ++count;
+        std::cin >> word;
+    }
+    std::wcout << L"Вы ввели " << count << L" слов.\n";
+
+
+    return 0;
+}
